10ab053bff69dc80348d3add79f087cc45adc863
RG-350 <blackz1982@yandex.ru>
2021-07-06 22:57:08 +0300

    DDR2_H5PS1G63EFR-S6C.h: Update timings and set CL=5
    Signed-off-by: RG-350 <blackz1982@yandex.ru>

--- include/asm-mips/jz_mem_nand_configs/DDR2_H5PS1G63EFR-S6C.h
+++ include/asm-mips/jz_mem_nand_configs/DDR2_H5PS1G63EFR-S6C.h
@@ -16,27 +16,27 @@
 #define DDR_ROW 13  /* ROW : 12 to 14 row address */
 #define DDR_COL 10  /* COL :  8 to 10 column address */
 #define DDR_BANK8 1 /* Banks each chip: 0-4bank, 1-8bank */
-#define DDR_CL    3 /* CAS latency: 1 to 7 */
+#define DDR_CL    5 /* CAS latency: 1 to 7 */
 
 /*
  * DDR2 controller timing1 register
  */
-#define DDR_tRAS 45  /* tRAS: ACTIVE to PRECHARGE command period to the same bank. */
-#define DDR_tRTP 8//10   /* 7.5ns READ to PRECHARGE command period. */
+#define DDR_tRAS 40  /* tRAS: ACTIVE to PRECHARGE command period to the same bank. */
+#define DDR_tRTP 7.5//10   /* 7.5ns READ to PRECHARGE command period. */
 #define DDR_tRP  15  /* tRP: PRECHARGE command period to the same bank */
 #define DDR_tRCD 15  /* ACTIVE to READ or WRITE command period to the same bank. */
-#define DDR_tRC  60  /* ACTIVE to ACTIVE command period to the same bank.*/
-#define DDR_tRRD 10//8  /* ACTIVE bank A to ACTIVE bank B command period. */
+#define DDR_tRC  55  /* ACTIVE to ACTIVE command period to the same bank.*/
+#define DDR_tRRD 7.5//8  /* ACTIVE bank A to ACTIVE bank B command period. */
 #define DDR_tWR  15//20  /* WRITE Recovery Time defined by register MR of DDR2 memory */
-#define DDR_tWTR 10  /* WRITE to READ command delay. */
+#define DDR_tWTR 7.5  /* WRITE to READ command delay. */
 
 /*
  * DDR2 controller timing2 register
  */
-#define DDR_tRFC 128 /* ns,  AUTO-REFRESH command period. */
-#define DDR_tMINSR 138//6 /* Minimum Self-Refresh / tXSNR */
+#define DDR_tRFC 75 /* ns,  AUTO-REFRESH command period. */
+#define DDR_tMINSR 140//6 /* Minimum Self-Refresh / tXSNR */
 #define DDR_tXP 2    /* tCK EXIT-POWER-DOWN to next valid command period: 1 to 8 tCK. */
-#define DDR_tMRD 2   /* unit: tCK. Load-Mode-Register to next valid command period: 1 to 4 tCK */
+#define DDR_tMRD 4   /* unit: tCK. Load-Mode-Register to next valid command period: 1 to 4 tCK */
 
 /*
  * DDR2 controller refcnt register
